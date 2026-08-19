a59a55fbb5709c2f92731bc22d20ef097ff22d28
Psych0p3st <psych0p3st@mail.ru>
2026-08-19 20:03:09 +0300

    Speed+Calc+Hello(Test+install)

--- Stepik/Speedometr/Speed.go
+++ Stepik/Speedometr/Speed.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    var dist float64
-    var time int
-    fmt.Scan(&dist, &time)
-    fmt.Println(dist/ float64(time))
-	
+	var dist float64
+	var time int
+	fmt.Scan(&dist, &time)
+	fmt.Println(dist / float64(time))
+
 }
